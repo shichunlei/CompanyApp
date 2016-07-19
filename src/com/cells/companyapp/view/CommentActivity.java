0cc0b31f29e4159eeb54b7ed4696eeb4c55ff15e
shichunlei <1558053958@qq.com>
2016-07-19 09:38:33 +0800

    修改评论列表的数据展示

--- src/com/cells/companyapp/view/CommentActivity.java
+++ src/com/cells/companyapp/view/CommentActivity.java
@@ -1,12 +1,7 @@
 package com.cells.companyapp.view;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import scl.leo.library.dialog.circularprogress.CircularProgressDialog;
-import scl.leo.library.utils.other.JsonUtil;
-import scl.leo.library.utils.other.SPUtils;
-import scl.leo.library.utils.other.StringUtil;
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.FinalHttp;
 import net.tsz.afinal.annotation.view.ViewInject;
@@ -21,12 +16,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cells.companyapp.R;
-import com.cells.companyapp.adapter.CommentAdapter;
 import com.cells.companyapp.base.BaseActivity;
+import com.cells.companyapp.base.BaseAdapterHelper;
+import com.cells.companyapp.base.CommonAdapter;
 import com.cells.companyapp.been.Comment;
 import com.cells.companyapp.been.Result;
-import com.cells.companyapp.utils.AppConfig;
-import com.cells.companyapp.utils.HttpUtils;
+import com.cells.companyapp.customview.CircularProgressDialog;
+import com.cells.companyapp.utils.*;
 import com.google.gson.reflect.TypeToken;
 
 public class CommentActivity extends BaseActivity {
@@ -45,8 +41,6 @@ public class CommentActivity extends BaseActivity {
 	private EditText etBody;
 
 	private List<Comment> comment;
-	private List<Comment> commentList = new ArrayList<Comment>();
-	private CommentAdapter adapter;
 
 	private CircularProgressDialog loading;
 
@@ -84,12 +78,9 @@ public class CommentActivity extends BaseActivity {
 			add_comment = HttpUtils.WINDOW_ADD_COMMENT;
 		}
 
-		status = (Integer) SPUtils.get(context, "login_status", 0,
-				AppConfig.LOGIN_STATUS_DATA);
-		user_id = (Integer) SPUtils.get(context, "id", 0,
-				AppConfig.LOGIN_INFO_DATA);
-		token = (String) SPUtils.get(context, "token", "",
-				AppConfig.LOGIN_INFO_DATA);
+		status = (Integer) SPUtils.get(context, "login_status", 0, AppConfig.LOGIN_STATUS_DATA);
+		user_id = (Integer) SPUtils.get(context, "id", 0, AppConfig.LOGIN_INFO_DATA);
+		token = (String) SPUtils.get(context, "token", "", AppConfig.LOGIN_INFO_DATA);
 
 		tvTitle.setText("评论");
 		back.setImageResource(R.drawable.icon_back);
@@ -103,54 +94,57 @@ public class CommentActivity extends BaseActivity {
 	private void getCommentList(int id, String comment_list) {
 		AjaxParams params = new AjaxParams();
 
-		params.put("id", id + "");
+		params.put("id", id);
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.get(HttpUtils.ROOT_URL + comment_list, params,
-				new AjaxCallBack<Object>() {
+		fh.get(HttpUtils.ROOT_URL + comment_list, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@SuppressWarnings("unchecked")
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						loading.dismiss();
+			@SuppressWarnings("unchecked")
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				loading.dismiss();
 
-						comment = (List<Comment>) JsonUtil.fromJson(str,
-								new TypeToken<List<Comment>>() {
-								});
-
-						if (commentList != null) {
-							commentList.clear();
-						}
-						if (comment.size() > 0) {
-							commentList.addAll(comment);
-							comment.clear();
-						} else {
-							showToast("没有评论！");
-						}
-
-						adapter = new CommentAdapter(context);
-						adapter.replaceWith(commentList);
-						listview.setAdapter(adapter);
-					}
-
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						loading.dismiss();
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
+				comment = (List<Comment>) JsonUtil.fromJson(str, new TypeToken<List<Comment>>() {
 				});
+
+				if (comment.size() > 0) {
+					listview.setAdapter(new CommonAdapter<Comment>(context, R.layout.item_comment, comment) {
+
+						@Override
+						public void onUpdate(BaseAdapterHelper helper, Comment item, int position) {
+							if (null != item.getUser_name()) {
+								helper.setText(R.id.tv_name, item.getUser_name());
+								helper.setHeadPicUrl(context, R.id.image_headpic, item.getUser_avatar());
+							} else {
+								helper.setText(R.id.tv_name, item.getManager_name());
+								helper.setHeadPicUrl(context, R.id.image_headpic, item.getManager_avatar());
+							}
+							helper.setText(R.id.tv_body, item.getBody());
+							helper.setText(R.id.tv_createdAt, item.getCreated_at());
+						}
+					});
+				} else {
+					showToast("没有评论！");
+				}
+			}
+
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				loading.dismiss();
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	public void send(View v) {
@@ -163,51 +157,49 @@ public class CommentActivity extends BaseActivity {
 		}
 	}
 
-	private void sendComment(final int id, String body, String add_comment,
-			int user_id, String token) {
+	private void sendComment(final int id, String body, String add_comment, int user_id, String token) {
 		AjaxParams params = new AjaxParams();
-		params.put("id", id + "");
+		params.put("id", id);
 		params.put("content", body);
 		if (status == 1) {
-			params.put("user_id", user_id + "");
+			params.put("user_id", user_id);
 		} else if (status == 2) {
-			params.put("manager_id", user_id + "");
+			params.put("manager_id", user_id);
 		}
 		params.put("auth_token", token);
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.post(HttpUtils.ROOT_URL + add_comment, params,
-				new AjaxCallBack<Object>() {
+		fh.post(HttpUtils.ROOT_URL + add_comment, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						result = (Result) JsonUtil.fromJson(str, Result.class);
-						if (result.isStatus()) {
-							getCommentList(id, comment_list);
-						} else {
-							loading.dismiss();
-							showToast("faulse");
-						}
-					}
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				result = (Result) JsonUtil.fromJson(str, Result.class);
+				if (result.isStatus()) {
+					etBody.setText("");
+					getCommentList(id, comment_list);
+				} else {
+					loading.dismiss();
+					showToast("faulse");
+				}
+			}
 
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						loading.dismiss();
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
-				});
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				loading.dismiss();
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	/**
--- src/com/cells/companyapp/view/CultrueInfoActivity.java
+++ src/com/cells/companyapp/view/CultrueInfoActivity.java
@@ -1,12 +1,10 @@
 package com.cells.companyapp.view;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import scl.leo.library.dialog.AlertDialog;
-import scl.leo.library.dialog.circularprogress.CircularProgressDialog;
-import scl.leo.library.utils.other.JsonUtil;
-import scl.leo.library.utils.other.SPUtils;
+import com.cells.companyapp.customview.CircularProgressDialog;
+import com.cells.companyapp.utils.*;
+
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.FinalBitmap;
 import net.tsz.afinal.FinalHttp;
@@ -21,12 +19,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cells.companyapp.R;
-import com.cells.companyapp.adapter.CommentAdapter;
 import com.cells.companyapp.base.BaseActivity;
+import com.cells.companyapp.base.BaseAdapterHelper;
+import com.cells.companyapp.base.CommonAdapter;
 import com.cells.companyapp.been.Collection;
 import com.cells.companyapp.been.Comment;
 import com.cells.companyapp.been.Culture;
 import com.cells.companyapp.been.Result;
+import com.cells.companyapp.customview.AlertDialog;
 import com.cells.companyapp.customview.MyListView;
 import com.cells.companyapp.utils.AppConfig;
 import com.cells.companyapp.utils.DBUtils;
@@ -72,8 +72,6 @@ public class CultrueInfoActivity extends BaseActivity {
 	private MyListView listview;
 
 	private List<Comment> comment;
-	private List<Comment> commentList = new ArrayList<Comment>();
-	private CommentAdapter adapter;
 
 	/** 点赞状态 */
 	private boolean like = false;
@@ -135,10 +133,8 @@ public class CultrueInfoActivity extends BaseActivity {
 			title = "标志释义";
 		}
 
-		status = (Integer) SPUtils.get(context, "login_status", 0,
-				AppConfig.LOGIN_STATUS_DATA);
-		user_id = (Integer) SPUtils.get(context, "id", 0,
-				AppConfig.LOGIN_INFO_DATA);
+		status = (Integer) SPUtils.get(context, "login_status", 0, AppConfig.LOGIN_STATUS_DATA);
+		user_id = (Integer) SPUtils.get(context, "id", 0, AppConfig.LOGIN_INFO_DATA);
 
 		tvTitle.setText(title);
 		back.setImageResource(R.drawable.icon_back);
@@ -161,119 +157,120 @@ public class CultrueInfoActivity extends BaseActivity {
 
 	private void getCommentList(int id) {
 		AjaxParams params = new AjaxParams();
-		params.put("id", id + "");
+		params.put("id", id);
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.get(HttpUtils.ROOT_URL + HttpUtils.CULTURE_COMMENTS, params,
-				new AjaxCallBack<Object>() {
+		fh.get(HttpUtils.ROOT_URL + HttpUtils.CULTURE_COMMENTS, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@SuppressWarnings("unchecked")
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						loading.dismiss();
+			@SuppressWarnings("unchecked")
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				loading.dismiss();
 
-						comment = (List<Comment>) JsonUtil.fromJson(str,
-								new TypeToken<List<Comment>>() {
-								});
-
-						if (commentList != null) {
-							commentList.clear();
-						}
-						if (comment.size() > 0) {
-							commentList.addAll(comment);
-							comment.clear();
-						}
-
-						adapter = new CommentAdapter(context);
-						adapter.replaceWith(commentList);
-						listview.setAdapter(adapter);
-					}
-
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						loading.dismiss();
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
+				comment = (List<Comment>) JsonUtil.fromJson(str, new TypeToken<List<Comment>>() {
 				});
+
+				if (comment.size() > 0) {
+					listview.setAdapter(new CommonAdapter<Comment>(context, R.layout.item_comment, comment) {
+
+						@Override
+						public void onUpdate(BaseAdapterHelper helper, Comment item, int position) {
+							if (null != item.getUser_name()) {
+								helper.setText(R.id.tv_name, item.getUser_name());
+								helper.setHeadPicUrl(context, R.id.image_headpic, item.getUser_avatar());
+							} else {
+								helper.setText(R.id.tv_name, item.getManager_name());
+								helper.setHeadPicUrl(context, R.id.image_headpic, item.getManager_avatar());
+							}
+							helper.setText(R.id.tv_body, item.getBody());
+							helper.setText(R.id.tv_createdAt, item.getCreated_at());
+						}
+					});
+				} else {
+
+				}
+			}
+
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				loading.dismiss();
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	private void getCultureInfo(final int id, int type, int user_id) {
 		AjaxParams params = new AjaxParams();
 		if (status == 1) {
-			params.put("user_id", user_id + "");
+			params.put("user_id", user_id);
 		} else if (status == 2) {
-			params.put("manager_id", user_id + "");
+			params.put("manager_id", user_id);
 		} else if (status == 0) {
 			if (user_id != 0) {
-				params.put("user_id", user_id + "");
+				params.put("user_id", user_id);
 			}
 		}
-		params.put("id", id + "");
+		params.put("id", id);
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.get(HttpUtils.ROOT_URL + culture_info_type, params,
-				new AjaxCallBack<Object>() {
+		fh.get(HttpUtils.ROOT_URL + culture_info_type, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						culture = (Culture) JsonUtil.fromJson(str,
-								Culture.class);
-						loading.dismiss();
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				culture = (Culture) JsonUtil.fromJson(str, Culture.class);
+				loading.dismiss();
 
-						tv_comment_count.setText(culture.getComment_count()
-								+ "");
-						tv_like_count.setText(culture.getVote_count() + "");
+				tv_comment_count.setText(culture.getComment_count() + "");
+				tv_like_count.setText(culture.getVote_count() + "");
 
-						if (culture.isLike_status()) {
-							like = true;
-							img_like.setImageResource(R.drawable.icon_like);
-						} else {
-							like = false;
-							img_like.setImageResource(R.drawable.icon_unlike);
-						}
+				if (culture.isLike_status()) {
+					like = true;
+					img_like.setImageResource(R.drawable.icon_like);
+				} else {
+					like = false;
+					img_like.setImageResource(R.drawable.icon_unlike);
+				}
 
-						FinalBitmap fb = FinalBitmap.create(context);
-						fb.display(logo, culture.getLogo());
-						if (null != culture.getName()) {
-							name.setText(culture.getName());
-						}
-						fb.display(image, culture.getImage());
-						if (null != culture.getContent()) {
-							content.setText(culture.getContent());
-						}
-						getCommentList(id);
-					}
+				FinalBitmap fb = FinalBitmap.create(context);
+				fb.display(logo, culture.getLogo());
+				if (null != culture.getName()) {
+					name.setText(culture.getName());
+				}
+				fb.display(image, culture.getImage());
+				if (null != culture.getContent()) {
+					content.setText(culture.getContent());
+				}
+				getCommentList(id);
+			}
 
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-							loading.dismiss();
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
-				});
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+					loading.dismiss();
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	public void back(View v) {
@@ -301,45 +298,43 @@ public class CultrueInfoActivity extends BaseActivity {
 	 */
 	private void addLike(final int id, final int user_id) {
 		AjaxParams params = new AjaxParams();
-		params.put("id", id + "");
+		params.put("id", id);
 		if (status == 1 || status == 0) {
-			params.put("user_id", user_id + "");
+			params.put("user_id", user_id);
 		} else if (status == 2) {
-			params.put("manager_id", user_id + "");
+			params.put("manager_id", user_id);
 		}
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.post(HttpUtils.ROOT_URL + HttpUtils.CULTURE_LIKE, params,
-				new AjaxCallBack<Object>() {
+		fh.post(HttpUtils.ROOT_URL + HttpUtils.CULTURE_LIKE, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						result = new Result();
-						result = (Result) JsonUtil.fromJson(str, Result.class);
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				result = new Result();
+				result = (Result) JsonUtil.fromJson(str, Result.class);
 
-						if (result.isStatus()) {
-							getCultureInfo(id, type, user_id);
-						}
-					}
+				if (result.isStatus()) {
+					getCultureInfo(id, type, user_id);
+				}
+			}
 
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						loading.dismiss();
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
-				});
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				loading.dismiss();
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	/**
@@ -350,44 +345,42 @@ public class CultrueInfoActivity extends BaseActivity {
 	 */
 	private void delLike(final int id, final int user_id) {
 		AjaxParams params = new AjaxParams();
-		params.put("id", id + "");
+		params.put("id", id);
 		if (status == 1 || status == 0) {
-			params.put("user_id", user_id + "");
+			params.put("user_id", user_id);
 		} else if (status == 2) {
-			params.put("manager_id", user_id + "");
+			params.put("manager_id", user_id);
 		}
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.post(HttpUtils.ROOT_URL + HttpUtils.CULTURE_UNLIKE, params,
-				new AjaxCallBack<Object>() {
+		fh.post(HttpUtils.ROOT_URL + HttpUtils.CULTURE_UNLIKE, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						result = new Result();
-						result = (Result) JsonUtil.fromJson(str, Result.class);
-						if (result.isStatus()) {
-							getCultureInfo(id, type, user_id);
-						}
-					}
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				result = new Result();
+				result = (Result) JsonUtil.fromJson(str, Result.class);
+				if (result.isStatus()) {
+					getCultureInfo(id, type, user_id);
+				}
+			}
 
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						loading.dismiss();
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
-				});
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				loading.dismiss();
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	public void comment(View v) {
@@ -399,24 +392,19 @@ public class CultrueInfoActivity extends BaseActivity {
 	}
 
 	private void showDialog() {
-		new AlertDialog(context)
-				.builder()
-				.setTitle(getString(R.string.remind))
+		new AlertDialog(context).builder().setTitle(getString(R.string.remind))
 				.setMsg(getString(R.string.dialog_title))
-				.setPositiveButton(getString(R.string.personal_login),
-						new OnClickListener() {
-							@Override
-							public void onClick(View v) {
-								openActivity(PersonalLoginActivity.class, false);
-							}
-						})
-				.setNegativeButton(getString(R.string.company_login),
-						new OnClickListener() {
-							@Override
-							public void onClick(View v) {
-								openActivity(CompanyLoginActivity.class, false);
-							}
-						}).show();
+				.setPositiveButton(getString(R.string.personal_login), new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						openActivity(PersonalLoginActivity.class, false);
+					}
+				}).setNegativeButton(getString(R.string.company_login), new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						openActivity(CompanyLoginActivity.class, false);
+					}
+				}).show();
 	}
 
 	public void collection(View v) {
