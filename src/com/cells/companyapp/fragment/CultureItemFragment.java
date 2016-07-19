65c7f8d28934d3184682ef01dc63764ccba02610
shichunlei <1558053958@qq.com>
2016-07-19 08:52:28 +0800

    修改企业画刊、企业文化中心的数据显示

--- src/com/cells/companyapp/fragment/CultureItemFragment.java
+++ src/com/cells/companyapp/fragment/CultureItemFragment.java
@@ -2,8 +2,8 @@ package com.cells.companyapp.fragment;
 
 import java.util.List;
 
-import scl.leo.library.dialog.circularprogress.CircularProgressDialog;
-import scl.leo.library.utils.other.JsonUtil;
+import com.cells.companyapp.utils.JsonUtil;
+
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.FinalHttp;
 import net.tsz.afinal.annotation.view.ViewInject;
@@ -14,14 +14,15 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 
 import com.cells.companyapp.R;
-import com.cells.companyapp.adapter.CultureAdapter;
+import com.cells.companyapp.base.BaseAdapterHelper;
 import com.cells.companyapp.base.BaseFragment;
+import com.cells.companyapp.base.CommonAdapter;
 import com.cells.companyapp.been.Culture;
+import com.cells.companyapp.customview.CircularProgressDialog;
 import com.cells.companyapp.customview.refresh.XListView;
 import com.cells.companyapp.customview.refresh.XListView.FooterListener;
 import com.cells.companyapp.customview.refresh.XListView.HeaderListener;
@@ -29,13 +30,12 @@ import com.cells.companyapp.utils.HttpUtils;
 import com.cells.companyapp.view.CultrueInfoActivity;
 import com.google.gson.reflect.TypeToken;
 
-public class CultureItemFragment extends BaseFragment implements
-		FooterListener, HeaderListener {
+public class CultureItemFragment extends BaseFragment implements FooterListener, HeaderListener {
 
 	@ViewInject(id = R.id.xlistview)
 	private XListView listview;
 
-	private CultureAdapter adapter;
+	private CommonAdapter<Culture> adapter;
 
 	private List<Culture> culture;
 
@@ -50,13 +50,10 @@ public class CultureItemFragment extends BaseFragment implements
 	private CircularProgressDialog loading;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.fragment_culture_items, container,
-				false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		view = inflater.inflate(R.layout.fragment_culture_items, container, false);
 		FinalActivity.initInjectedView(this, view);
 		init();
-		setItemClick();
 		return view;
 	}
 
@@ -65,64 +62,82 @@ public class CultureItemFragment extends BaseFragment implements
 		listview.setXHeaderListener(this);
 		listview.setXFooterListener(this);
 		listview.setSelector(new ColorDrawable(Color.TRANSPARENT));
-		adapter = new CultureAdapter(getActivity());
 
 		loading = CircularProgressDialog.show(getActivity());
 	}
 
-	private void getCultureList(int page, String culture_type, final int type) {
+	private void getCultureList(int page, String url, final int type) {
 		AjaxParams params = new AjaxParams();
 		params.put("page", page + "");
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.get(HttpUtils.ROOT_URL + culture_type, params,
-				new AjaxCallBack<Object>() {
+		fh.get(HttpUtils.ROOT_URL + url, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@SuppressWarnings("unchecked")
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						loading.dismiss();
-						culture = (List<Culture>) JsonUtil.fromJson(str,
-								new TypeToken<List<Culture>>() {
-								});
-
-						if (type == 1) {
-							adapter.clear();
-							adapter.addItemTop(culture);
-							adapter.notifyDataSetChanged();
-							listview.stopRefresh();
-							listview.setRefreshTime("刚刚");
-						} else if (type == 2) {
-							adapter.addItemLast(culture);
-							adapter.notifyDataSetChanged();
-							listview.stopLoadMore();
-						}
-					}
-
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-							loading.dismiss();
-							if (type == 2) {
-								listview.stopLoadMore();
-							} else if (type == 1) {
-								listview.stopRefresh();
-								listview.setRefreshTime("刚刚");
-							}
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
+			@SuppressWarnings("unchecked")
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				loading.dismiss();
+				culture = (List<Culture>) JsonUtil.fromJson(str, new TypeToken<List<Culture>>() {
 				});
+
+				if (type == 1) {
+					adapter.clear();
+					adapter.addAll(culture);
+					listview.stopRefresh();
+					listview.setRefreshTime("刚刚");
+				} else if (type == 2) {
+					adapter.addAll(culture);
+					listview.stopLoadMore();
+				} else if (type == 0) {
+					if (culture.size() > 0) {
+						adapter = new CommonAdapter<Culture>(context, R.layout.item_culture, culture) {
+
+							@Override
+							public void onUpdate(BaseAdapterHelper helper, final Culture item, int position) {
+								helper.setText(R.id.tv_name, item.getName());
+								helper.setText(R.id.tv_content, item.getContent());
+								helper.setImageUrl(context, R.id.img_culture, item.getLogo());
+								helper.setOnClickListener(R.id.layout_culture, new OnClickListener() {
+
+									@Override
+									public void onClick(View v) {
+										Bundle bundle = new Bundle();
+										bundle.putInt("id", item.getId());
+										bundle.putInt("type", culture_type);
+
+										openActivity(CultrueInfoActivity.class, bundle, false);
+									}
+								});
+							}
+						};
+						listview.setAdapter(adapter);
+					}
+				}
+			}
+
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+					loading.dismiss();
+					if (type == 2) {
+						listview.stopLoadMore();
+					} else if (type == 1) {
+						listview.stopRefresh();
+						listview.setRefreshTime("刚刚");
+					}
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	@Override
@@ -158,27 +173,9 @@ public class CultureItemFragment extends BaseFragment implements
 
 		if (!isfirst) {
 			page = 1;
-			listview.setAdapter(adapter);
 			loading.show();
-			getCultureList(page, CULTURE, 2);
+			getCultureList(page, CULTURE, 0);
 		}
 		isfirst = true;
-	}
-
-	private void setItemClick() {
-		listview.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				int _id = ((Culture) adapter.getItem(position - 1)).getId();
-
-				Bundle bundle = new Bundle();
-				bundle.putInt("id", _id);
-				bundle.putInt("type", culture_type);
-
-				openActivity(CultrueInfoActivity.class, bundle, false);
-			}
-		});
 	}
 }
--- src/com/cells/companyapp/fragment/VideoItemFragment.java
+++ src/com/cells/companyapp/fragment/VideoItemFragment.java
@@ -7,20 +7,22 @@ import net.tsz.afinal.FinalHttp;
 import net.tsz.afinal.annotation.view.ViewInject;
 import net.tsz.afinal.http.AjaxCallBack;
 import net.tsz.afinal.http.AjaxParams;
-import scl.leo.library.dialog.circularprogress.CircularProgressDialog;
-import scl.leo.library.utils.other.JsonUtil;
+
+import com.cells.companyapp.utils.JsonUtil;
+import com.cells.companyapp.customview.CircularProgressDialog;
+
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 
 import com.cells.companyapp.R;
-import com.cells.companyapp.adapter.VideoAdapter;
+import com.cells.companyapp.base.BaseAdapterHelper;
 import com.cells.companyapp.base.BaseFragment;
+import com.cells.companyapp.base.CommonAdapter;
 import com.cells.companyapp.been.Video;
 import com.cells.companyapp.customview.refresh.XListView;
 import com.cells.companyapp.customview.refresh.XListView.FooterListener;
@@ -29,13 +31,12 @@ import com.cells.companyapp.utils.HttpUtils;
 import com.cells.companyapp.view.VideoInfoActivity;
 import com.google.gson.reflect.TypeToken;
 
-public class VideoItemFragment extends BaseFragment implements FooterListener,
-		HeaderListener {
+public class VideoItemFragment extends BaseFragment implements FooterListener, HeaderListener {
 
 	@ViewInject(id = R.id.xlistview)
 	private XListView listview;
 
-	private VideoAdapter adapter;
+	private CommonAdapter<Video> adapter;
 
 	private List<Video> culture;
 
@@ -48,13 +49,10 @@ public class VideoItemFragment extends BaseFragment implements FooterListener,
 	private CircularProgressDialog loading;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		view = inflater
-				.inflate(R.layout.fragment_video_items, container, false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		view = inflater.inflate(R.layout.fragment_video_items, container, false);
 		FinalActivity.initInjectedView(this, view);
 		init();
-		setItemClick();
 		return view;
 	}
 
@@ -63,65 +61,79 @@ public class VideoItemFragment extends BaseFragment implements FooterListener,
 		listview.setXHeaderListener(this);
 		listview.setXFooterListener(this);
 		listview.setSelector(new ColorDrawable(Color.TRANSPARENT));
-		adapter = new VideoAdapter(getActivity(), listview);
 
 		loading = CircularProgressDialog.show(getActivity());
 	}
 
 	private void getCultureList(int page, int video_type, final int type) {
 		AjaxParams params = new AjaxParams();
-		params.put("page", page + "");
-		params.put("v_type", video_type + "");
+		params.put("page", page);
+		params.put("v_type", video_type);
 
 		FinalHttp fh = new FinalHttp();
 		fh.configTimeout(HttpUtils.TIME_OUT);
-		fh.get(HttpUtils.ROOT_URL + HttpUtils.VIDEOS, params,
-				new AjaxCallBack<Object>() {
+		fh.get(HttpUtils.ROOT_URL + HttpUtils.VIDEOS, params, new AjaxCallBack<Object>() {
 
-					@Override
-					public void onLoading(long count, long current) {
-						super.onLoading(count, current);
-					}
+			@Override
+			public void onLoading(long count, long current) {
+				super.onLoading(count, current);
+			}
 
-					@SuppressWarnings("unchecked")
-					@Override
-					public void onSuccess(Object t) {
-						super.onSuccess(t);
-						String str = t.toString();
-						loading.dismiss();
-						culture = (List<Video>) JsonUtil.fromJson(str,
-								new TypeToken<List<Video>>() {
-								});
-
-						if (type == 1) {
-							adapter.clear();
-							adapter.addItemTop(culture);
-							adapter.notifyDataSetChanged();
-							listview.stopRefresh();
-							listview.setRefreshTime("刚刚");
-						} else if (type == 2) {
-							adapter.addItemLast(culture);
-							adapter.notifyDataSetChanged();
-							listview.stopLoadMore();
-						}
-					}
-
-					@Override
-					public void onFailure(Throwable t, int errorNo,
-							String strMsg) {
-						if (t != null) {
-							showToast("加载失败，请稍后再试！");
-							loading.dismiss();
-							if (type == 2) {
-								listview.stopLoadMore();
-							} else if (type == 1) {
-								listview.stopRefresh();
-								listview.setRefreshTime("刚刚");
-							}
-						}
-						super.onFailure(t, errorNo, strMsg);
-					}
+			@SuppressWarnings("unchecked")
+			@Override
+			public void onSuccess(Object t) {
+				super.onSuccess(t);
+				String str = t.toString();
+				loading.dismiss();
+				culture = (List<Video>) JsonUtil.fromJson(str, new TypeToken<List<Video>>() {
 				});
+
+				if (type == 1) {
+					adapter.clear();
+					adapter.addAll(culture);
+					listview.stopRefresh();
+					listview.setRefreshTime("刚刚");
+				} else if (type == 2) {
+					adapter.addAll(culture);
+					listview.stopLoadMore();
+				} else if (type == 0) {
+					adapter = new CommonAdapter<Video>(context, R.layout.item_video, culture) {
+
+						@Override
+						public void onUpdate(BaseAdapterHelper helper, final Video item, int position) {
+							helper.setText(R.id.tv_name, item.getName());
+							helper.setImageUrl(context, R.id.img_video, item.getImage());
+							helper.setOnClickListener(R.id.layout_video, new OnClickListener() {
+
+								@Override
+								public void onClick(View v) {
+									Bundle bundle = new Bundle();
+									bundle.putInt("id", item.getId());
+									bundle.putString("name", item.getName());
+									openActivity(VideoInfoActivity.class, bundle, false);
+								}
+							});
+						}
+					};
+					listview.setAdapter(adapter);
+				}
+			}
+
+			@Override
+			public void onFailure(Throwable t, int errorNo, String strMsg) {
+				if (t != null) {
+					showToast("加载失败，请稍后再试！");
+					loading.dismiss();
+					if (type == 2) {
+						listview.stopLoadMore();
+					} else if (type == 1) {
+						listview.stopRefresh();
+						listview.setRefreshTime("刚刚");
+					}
+				}
+				super.onFailure(t, errorNo, strMsg);
+			}
+		});
 	}
 
 	@Override
@@ -142,28 +154,9 @@ public class VideoItemFragment extends BaseFragment implements FooterListener,
 		video_type = getArguments().getInt("type");
 		if (!isfirst) {
 			page = 1;
-			listview.setAdapter(adapter);
 			loading.show();
-			getCultureList(page, video_type, 2);
+			getCultureList(page, video_type, 0);
 		}
 		isfirst = true;
-	}
-
-	private void setItemClick() {
-		listview.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				int _id = ((Video) adapter.getItem(position - 1)).getId();
-				String name = ((Video) adapter.getItem(position - 1)).getName();
-
-				Bundle bundle = new Bundle();
-				bundle.putInt("id", _id);
-				bundle.putString("name", name);
-
-				openActivity(VideoInfoActivity.class, bundle, false);
-			}
-		});
 	}
 }
